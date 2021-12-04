4275643e6655455fe9830cc669efd9f39e117509
sbtah <shisastore@protonmail.com>
2021-12-04 20:56:45 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let mesageElem = document.querySelector(".message");
+let numberElem = document.querySelector(".number");
+let scoreElem = document.querySelector(".score");
+let inputElem = document.querySelector(".guess");
+let btnElem = document.querySelector(".btn check");
+
+mesageElem.textContent = "☑️ Correct Number";
+numberElem.textContent = "1";
+scoreElem.textContent = "133";
