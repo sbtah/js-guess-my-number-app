74adae293e12c2015122d42ba60f93d61f9b174f
sbtah <shisastore@protonmail.com>
2021-12-04 22:16:35 +0100

    Added almost all logic

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 "use strict";
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let mesageElem = document.querySelector(".message");
 let numberElem = document.querySelector(".number");
 let scoreElem = document.querySelector(".score");
-let inputElem = document.querySelector(".guess");
-let btnElem = document.querySelector(".btn check");
+let highScoreElem = document.querySelector(".highscore");
+const inputElem = document.querySelector(".guess");
+const btnElem = document.querySelector(".check");
 
-mesageElem.textContent = "☑️ Correct Number";
-numberElem.textContent = "1";
-scoreElem.textContent = "133";
+console.log(secretNumber);
+
+btnElem.addEventListener("click", function () {
+	const guess = Number(inputElem.value);
+	if (!guess) {
+		mesageElem.textContent = "You need to type a number!";
+	} else if (guess === secretNumber) {
+		mesageElem.textContent = "You win!";
+		highScoreElem.textContent = scoreElem.textContent;
+		numberElem.textContent = secretNumber;
+	} else if (guess < secretNumber) {
+		mesageElem.textContent = "Guess higher!";
+		scoreElem.textContent -= 1;
+	} else if (guess > secretNumber) {
+		mesageElem.textContent = "Guess lower!";
+		scoreElem.textContent -= 1;
+	}
+});
